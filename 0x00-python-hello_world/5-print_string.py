0777f55d957d46f6099ae449e7d4bdd6c026b747
ZAKARIA <zakariaelaro@gmail.com>
2023-06-05 05:46:05 -0700

    Task5 Print String

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str[:9],str[10:] + str[:9],str[10:] + str[:9],str[10:]) 
-print(str[:9]) #by Calimero
+#print(str[:9],str[10:] + str[:9],str[10:] + str[:9],str[10:]) 
+#print(str[:9]) #by Calimero
+
+print(str + str + str)
+print(str[:9])
