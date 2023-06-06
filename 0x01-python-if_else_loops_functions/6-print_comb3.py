d2e671805d9acb93e87dc0a9f66206e4f5a8310d
ZAKARIA <zakariaelaro@gmail.com>
2023-06-06 10:54:36 -0700

    Task6 Check if good

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for j in range(0, 10, 1):
     for x in range(0, 10):
-        if j == x or j < x:
+        if j != x and j < x:
             print("{}{}".format(j, x), end='\n' if j == 8 and x == 9 else ', ')
