45c394764d71bb4f3a39de880db1d806eaad9110
ZAKARIA <zakariaelaro@gmail.com>
2023-06-07 03:04:00 -0700

    Check Task 9  There are only 3 colors,

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last_de = abs(number) % 10
-    return -last_de if number < 0 else last_de
+    last_di = abs(number) % 10
+    print(last_di)
