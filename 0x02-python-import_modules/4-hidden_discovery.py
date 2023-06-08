ad5c3adab99f899f55b94f39867c19ff0035b9ff
ZAKARIA <zakariaelaro@gmail.com>
2023-06-08 12:30:12 -0700

    check task4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import dis
+import hidden_4
+if __name__ == "__main__":
+    checkid = [checkid for checkid in dir(hidden_4)]
+    save_names = sorted(checkid)
+    for checkid in save_names:
+        if not checkid.startswith('__'):
+            print(checkid)
