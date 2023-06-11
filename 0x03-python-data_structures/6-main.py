65306357ee965cb76909936c966ad455d5c22889
ZAKARIA <zakariaelaro@gmail.com>
2023-06-11 06:09:39 -0700

    Check Task6 )

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
 matrix = [
@@ -6,3 +7,7 @@ matrix = [
     [4, 5, 6],
     [7, 8, 9]
 ]
+
+print_matrix_integer(matrix)
+print("--$")
+print_matrix_integer()
