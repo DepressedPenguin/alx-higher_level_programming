1bab700ca98e4522cb686738819567739ba38824
ZAKARIA <zakariaelaro@gmail.com>
2023-06-14 07:58:23 -0700

    Search and replace Task1

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 
 matrix = [
@@ -6,3 +7,7 @@ matrix = [
     [4, 5, 6],
     [7, 8, 9]
 ]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
