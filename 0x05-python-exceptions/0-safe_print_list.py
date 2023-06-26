6a8924c29e50bb81f72554bf13034b50289ef019
ZAKARIA <zakariaelaro@gmail.com>
2023-06-26 06:22:55 -0700

    check task 0 Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for numbers in my_list:
+            if count < x:
+                print(numbers, end='')
+                count += 1
+        print()
+    except:
+        pass
+    return count
