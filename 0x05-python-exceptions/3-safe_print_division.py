a9b0c900571830793a3dac35e1c7e0a35317a8e3
ZAKARIA <zakariaelaro@gmail.com>
2023-06-26 15:29:46 -0700

    Task 3 and hot sry i mean sahd

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        x = a / b
+    except (TypeError, ZeroDivisionError):
+        x = None
+    finally:
+        print("Inside result: {}".format(x))
+    return (x)
