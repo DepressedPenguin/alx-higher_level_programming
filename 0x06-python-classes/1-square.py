902ef51954bdeadbf6c70b981652729084aed9d1
ZAKARIA <zakariaelaro@gmail.com>
2023-06-27 14:32:32 -0700

    task1 check fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+# 1-square.py by Zakaria Elaroussi
+"""Defines a square """
+
 class Square:
-    def __init__(self, size):
-        self.__size = size
+      """Represents a square"""
+
+      def __init__(self, size):
+          """Initializing this square class
+	  Args: size - represnets the size of the square defined
+	  """
+
+          self.__size = size
