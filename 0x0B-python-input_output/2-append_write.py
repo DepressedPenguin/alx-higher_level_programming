901a07e633f508a6c03d7e1e6d30df46735244bb
ZAKARIA <zakariaelaro@gmail.com>
2023-07-11 10:58:39 -0700

    Task 2 check if task work

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Function that append"""
+"""Function that appends"""
 
 
 def append_write(filename="", text=""):
-    """ function append_write """
+    """Function that appends a string at the end of a text file"""
     with open(filename, "a", encoding="utf-8") as calimero_file:
         calimero_file.write(text)
         return calimero_file.tell()
