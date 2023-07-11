2e64a6cc5e00f53a739aa23f31259821696fde84
ZAKARIA <zakariaelaro@gmail.com>
2023-07-11 10:47:39 -0700

    Task 2 Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that append"""
+
+
+def append_write(filename="", text=""):
+    """ function append_write """
+    with open(filename, "a", encoding="utf-8") as calimero_file:
+        calimero_file.write(text)
+        return calimero_file.tell()
