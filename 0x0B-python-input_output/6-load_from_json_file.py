d0bb4af9589a94111479230c77d04c381876562f
ZAKARIA <zakariaelaro@gmail.com>
2023-07-11 12:29:34 -0700

    Task6 check if work

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,7 +5,7 @@ import json
 
 def load_from_json_file(filename):
     """convert it into something can Pc Understand )"""
-    with open(filename,"r", encoding='utf-8') as f:
+    with open(filename, "r", encoding='utf-8') as f:
         string = f.read()
         obj = json.loads(string)
         return obj
