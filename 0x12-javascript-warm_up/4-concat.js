c4665ced794692d117819343ea1d9ae4bbb91fbc
ZAKARIA <zakariaelaro@gmail.com>
2023-09-11 15:12:16 -0700

    Task4 fix

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-if (process.argv.length === 4) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else {
-  console.log('undefined is undefined');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
