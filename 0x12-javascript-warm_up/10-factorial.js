6f9f518adb90e88f8470bb3a5f5953c4a22c1b71
ZAKARIA <zakariaelaro@gmail.com>
2023-09-12 05:09:25 -0700

    Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function factorial(num) {
+  if (Number.isNaN(num) || num < 0) {
+    return 1;
+  } else if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+const num = Number.parseInt(process.argv[2]);
+
+console.log(factorial(num));
