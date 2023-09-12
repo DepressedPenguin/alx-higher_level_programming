a9783ea898dacbfa359db3dc65eb08c6f33af080
ZAKARIA <zakariaelaro@gmail.com>
2023-09-12 03:05:33 -0700

    rev task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const arg in args) {
-  console.log(args[arg]);
+const my_tools = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let line;
+for (line of my_tools) {
+  console.log(line);
 }
