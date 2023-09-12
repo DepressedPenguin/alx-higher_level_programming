0e7fe5bf6dbacdeb73f6447e1fee557bb27ed203
ZAKARIA <zakariaelaro@gmail.com>
2023-09-12 03:20:39 -0700

    Task 6 check

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const my_tools = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const myTools = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let line;
-for (line of my_tools) {
+for (line of myTools) {
   console.log(line);
 }
