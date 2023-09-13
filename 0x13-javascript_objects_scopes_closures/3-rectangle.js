00e36f3bbb72f205abdc0ddfc2b2a0968ca0ca54
ZAKARIA <zakariaelaro@gmail.com>
2023-09-13 03:37:33 -0700

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w <= 0 || h <= 0) {
       this.width = 0;
       this.height = 0;
@@ -10,7 +10,7 @@ class Rectangle {
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
