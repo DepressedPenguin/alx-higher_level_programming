d716f6647698b7d9474e320b7424661ddd190d76
ZAKARIA <zakariaelaro@gmail.com>
2023-09-14 03:31:50 -0700

    Check task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,32 +1,29 @@
 #!/usr/bin/node
 const qu = require('./5-square.js');
 class Square extends qu {
-	constructor(size){
-		super(size, size);
+  constructor (size) {
+    super(size, size);
+  }
 
-	}
-
-	charPrint(c){
-		let x,d;
-        if(c === undefined){
-			for(x=0;x < this.width;x++){
-				let tmp = '';
-				for(d=0;d < this.height;d++){
-					tmp += 'X';
-				}
-				console.log(tmp);
-		}
+  charPrint (c) {
+    let x, d;
+    if (c === undefined) {
+      for (x = 0; x < this.width; x++) {
+        let tmp = '';
+        for (d = 0; d < this.height; d++) {
+          tmp += 'X';
         }
-		else{
-		    for(x=0;x < this.width;x++){
-				let tmp = '';
-				for(d=0;d < this.height;d++){
-					tmp += c;
-				}
-				console.log(tmp);
-		}
-		}
-	}
-
+        console.log(tmp);
+      }
+    } else {
+      for (x = 0; x < this.width; x++) {
+        let tmp = '';
+        for (d = 0; d < this.height; d++) {
+          tmp += c;
+        }
+        console.log(tmp);
+      }
+    }
+  }
 }
 module.exports = Square;
