ea4edc981f01f1ea5f5c839f5f53c0d3213e4dd8
ZAKARIA <zakariaelaro@gmail.com>
2023-09-17 03:12:25 -0700

    Check Task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""List All states inside hbtn_0e_0_usa db """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         user=username,
+                         passwd=password,
+                         db=db_name,
+                         port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+
+    result = cur.fetchall()
+    for sta in result:
+        print(sta)
+
+    cur.close()
+    db.close()
