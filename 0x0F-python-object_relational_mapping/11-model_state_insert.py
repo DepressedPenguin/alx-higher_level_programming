0afd818b06cd78c01f46c1e0cef5c073bcc70a26
ZAKARIA <zakariaelaro@gmail.com>
2023-09-18 15:12:16 -0700

    Check Task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""INSERT ELEMENT INSIDE TABLE"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State  # Make sure to import your Base and State from the model_state modulei
+import sys
+
+if __name__ == "__main__":
+    connect = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=connect)
+    new_session = Session()
+
+    existing_state = new_session.query(State).filter(State.name == 'Louisiana').first()
+    if not existing_state:
+        add_to = State(name='Louisiana')
+        new_session.add(add_to)
+        new_session.commit()
+
+    ret = new_session.query(State).filter(State.name == 'Louisiana').first()
+    if ret:
+        print(ret.id)
+
