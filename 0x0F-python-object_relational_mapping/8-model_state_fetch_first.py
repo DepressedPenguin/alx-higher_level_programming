7e9dadbe6009783863c1850d60755d0034a010da
ZAKARIA <zakariaelaro@gmail.com>
2023-09-18 11:42:42 -0700

    fix task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == "__main__":
     connect = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-            (sys.argv[1], sys.argv[2], sys.argv[3],), pool_pre_ping=True)
+                            (sys.argv[1], sys.argv[2], sys.argv[3],), pool_pre_ping=True)
     Session = sessionmaker(connect)
     new_session = Session()
     first_result = new_session.query(State).first()
@@ -16,5 +16,3 @@ if __name__ == "__main__":
         print("Nothing")
     else:
         print(first_result.id, first_result.name, sep=': ')
-
-
