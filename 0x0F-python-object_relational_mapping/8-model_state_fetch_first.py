32cc8e7ad6704cd3f219bb44f87d84a5e3788ca7
ZAKARIA <zakariaelaro@gmail.com>
2023-09-18 11:49:57 -0700

    check task if good

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,8 +8,8 @@ from sqlalchemy import create_engine
 import sys
 
 if __name__ == "__main__":
-    connect = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                            (sys.argv[1], sys.argv[2], sys.argv[3],), pool_pre_ping=True)
+    connect = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3],), pool_pre_ping=True)
     Session = sessionmaker(connect)
     new_session = Session()
     first_result = new_session.query(State).first()
