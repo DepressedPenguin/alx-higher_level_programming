2257bdcea3fce21138b3a49e201aa34d59cbc7e3
ZAKARIA <zakariaelaro@gmail.com>
2023-09-19 11:59:18 -0700

    Check Task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+import sys
+
+if __name__ == "__main__":
+    connect = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    new_session = sessionmaker(bind=connect)
+    session = new_session()
+    state_to_update = session.query(State).filter(State.id == 2).first()
+
+    state_to_update.name = 'New Mexico'
+    session.commit()
+    "print(state_to_update.name)"
