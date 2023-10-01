19b3b15b8982f19c7b5ae77a47cbb1a706c61e36
ZAKARIA <zakariaelaro@gmail.com>
2023-10-01 00:59:55 -0700

    try task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Post with email"""
+import urllib.request
+import sys
+
+if __name__=="__main__":
+    url_email = sys.argv[3]
+    enc = url_email.encode('utf-8')
+    request = urllib.request.Request(sys.argv[1], data=enc, method='POST')
+    with urllib.request.urlopen(request) as re:
+        content = re.read().decode('ascii')
+        print(content)
