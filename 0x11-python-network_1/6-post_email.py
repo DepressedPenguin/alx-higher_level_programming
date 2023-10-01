1ee01ca8991aee8480faa1c9731b48a75f8d2b0a
ZAKARIA <zakariaelaro@gmail.com>
2023-10-01 04:20:35 -0700

    Checj Task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""Post meth with Email """
-
-import sys
+"""POST URL"""
 import requests
+import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    data = {'email': email}
-    response = requests.post(url, data=data)
-
-    content = response.text
-    print("Your email is:", content)
+    args = sys.argv
+    data = {'email': args[2]}
+    res = requests.post(args[1], data)
+    print(res.text)
