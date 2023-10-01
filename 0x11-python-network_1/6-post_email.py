a30654ae89835adfc8069887312331e8a76edf14
ZAKARIA <zakariaelaro@gmail.com>
2023-10-01 04:13:26 -0700

    Task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Post meth with Email """
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    
+    content = response.text
+    print("Your email is:", content)
