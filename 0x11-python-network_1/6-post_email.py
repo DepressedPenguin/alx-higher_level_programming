399fe2dd06af524866fab290c259a2ea95af27b0
ZAKARIA <zakariaelaro@gmail.com>
2023-10-01 04:17:45 -0700

    Check task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,9 +7,9 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    
+
     data = {'email': email}
     response = requests.post(url, data=data)
-    
+
     content = response.text
     print("Your email is:", content)
