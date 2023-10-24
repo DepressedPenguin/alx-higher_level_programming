7df740d02b7c690b0e5b9cfa4c0b71f25a65fe3b
ZAKARIA <zakariaelaro@gmail.com>
2023-10-24 11:52:09 -0700

    trying to fix task2 p2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,16 +2,21 @@
 
 const request = require('request');
 
-let url = 'https://alx-intranet.hbtn.io/status';
+if (process.argv.length !== 3) {
+  console.error("Usage: ./2-statuscode.js <URL>");
+  process.exit(1);
+}
+
+const url = process.argv[2];
 
 request(url, (error, response, body) => {
-	if (error) {
-		console.error(error);
-	} else {
-		if (response.statusCode === 200) {
-			console.log("Code: 200");
-		} else {
-			console.log("Code: 404");
-		}
-	}
+  if (error) {
+    console.error(error);
+  } else {
+    if (response.statusCode === 200) {
+      console.log(`Code: 200`);
+    } else {
+      console.log(`Code: ${response.statusCode}`);
+    }
+  }
 });
