587fcf5a211715ec033f56a579a96a1c96b9b464
ZAKARIA <zakariaelaro@gmail.com>
2023-10-24 12:05:15 -0700

    trying to fix task

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 if (process.argv.length !== 3) {
-  console.error("Usage: ./2-statuscode.js <URL>");
+  console.error('Usage: ./2-statuscode.js <url>');
   process.exit(1);
 }
 
@@ -14,9 +14,9 @@ request(url, (error, response, body) => {
     console.error(error);
   } else {
     if (response.statusCode === 200) {
-      console.log(`Code: 200`);
+      console.log("Code: 200");
     } else {
-      console.log(`Code: ${response.statusCode}`);
+      console.log("Code: 404");
     }
   }
 });
