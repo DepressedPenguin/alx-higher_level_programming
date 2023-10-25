cf494f5a6139d96b9aa17478db6a54de73776410
ZAKARIA <zakariaelaro@gmail.com>
2023-10-25 03:33:22 -0700

    trying to fix task Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,22 +1,5 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-if (process.argv.length !== 3) {
-  console.error('Usage: ./2-statuscode.js <url>');
-  process.exit(1);
-}
-
-const url = process.argv[2];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    if (response.statusCode === 200) {
-      console.log("Code: 200");
-    } else {
-      console.log("Code: 404");
-    }
-  }
+request.get(process.argv[2]).on('response', function (response) {
+	console.log('code: ' + response.statusCode);
 });
