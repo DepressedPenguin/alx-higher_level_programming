5d055c318dee725f8b0a8be3fbb1f9caf118762b
ZAKARIA <zakariaelaro@gmail.com>
2023-10-25 03:10:11 -0700

    trying to fix task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,20 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-function writeme (filename, string) {
-  fs.writeFile(filename, string, (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-}
-
-const filename = process.argv[2];
-const stringToWrite = process.argv[3];
-
-if (!filename || !stringToWrite) {
-  console.error('Usage: node script.js <filename> <string>');
-  process.exit(1);
-}
-
-writeme(filename, stringToWrite);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
