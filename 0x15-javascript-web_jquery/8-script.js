d75a823252133dbeb2eff572bb081b428a2d44d0
ZAKARIA <zakariaelaro@gmail.com>
2023-11-01 15:16:12 -0700

    refix task 8

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,11 +1,7 @@
-$(document).ready(function () {
-    $.get("https://swapi-api.alx-tools.com/api/films/?format=json", function(data) {
-        var titles = [];
-        
-        for (var i = 0; i < data.length; i++) {
-            titles.push(data[i].title);
-        }
-
-        $("#list_movies").text(titles);
+$.get("https://swapi-api.alx-tools.com/api/films/?format=json", function (data, status) {
+  if (status === 'success') {
+    data.results.forEach((movie) => {
+      $("#list_movies").append(`<li>${movie.title}</li>`);
     });
+  }
 });
